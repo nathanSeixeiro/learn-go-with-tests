3d5fca1d304517c31e7f2d0ac122111f15d387d7
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-08-01 14:26:18 +0000

    ensure func hello return hello to people or hello world in default

---
+++ hello/hello_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T){
+	t.Run("Say hello to people", func (t *testing.T){
+		got := Hello("Nathan")
+		want := "Hello, Nathan"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got,want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string){
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
