7f338d32dbf93f386fdb37d1fbd161415e9e6406
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-08-03 16:39:43 +0000

    refactor

--- Examples/hello/hello.go
+++ Examples/hello/hello.go
@@ -2,21 +2,34 @@ package main
 
 import "fmt"
 
-const engleshPrefix = "Hello, "
-const spanishPrefix = "Hola, "
-const deafultName = "world"
-const spanish = "Spanish"
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishPrefix = "Hello, "
+	spanishPrefix = "Hola, "
+	frenchPrefix  = "Bonjour, "
+	deafultName   = "world"
+)
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = deafultName
 	}
+	prefix := gettingPrefixLang(language)
+	return prefix + name
+}
 
-	if language == spanish {
-		return spanishPrefix + name
+func gettingPrefixLang(lang string) (prefix string){
+	switch lang {
+	case french:
+		prefix = frenchPrefix
+	case spanish:
+		prefix = spanishPrefix
+	default:
+		prefix = englishPrefix
 	}
-
-	return engleshPrefix + name
+	return
 }
 
 func main() {
